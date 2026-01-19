0b650d5b8e9f592cdc68464d887220137a47fbf8
Priyanshi Dimri <priyanshidimri4@gmail.com>
2026-01-19 13:33:59 +0530

    Rename the files and update

--- Unused/speech_to_text of sample audio.py
+++ Unused/speech_to_text of sample audio.py
@@ -9,11 +9,5 @@ print(chunks)
 with open("output.json","w") as f:
     json.dump(chunks,f)
     
-#  Whisper is an open source model that is developed by Open AI that is used for the automatic speech recognization.It do variouus tasks such as transcribe audio to text(mean convert the audio to text), translate speech(convert speech of one language into text to other),identify spoken language,detech voice activity(determine the speech is present in audio or not)
-# if you are doing the translation then dont used turbo model
 
-# JSON (JavaScript Object Notation) is a lightweight data format used for storing and exchanging structured information. It’s easy for humans to read and write, and easy for machines to parse and generate.
-# - Text-based: It represents data as plain text.
-# - Structured: Uses key-value pairs and arrays.
-# - Language-independent: Though derived from JavaScript, it's supported by nearly every programming language.
  
